9a21684ce7ff46aa64017c4ea495d33db9b791a8
Amruth-github <79594127+Amruth-github@users.noreply.github.com>
2022-09-24 11:05:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,70 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox
 import threading as td
 from GraphPage import GraphPage
 from get_resource import *
 from mplcursors import cursor
+
+def PrepareTab(Tab : str):
+    tabsys1 = ttk.Notebook(tabsys) #Tab system in the main parent tab
+    tabsys.add(tabsys1, text = Tab)
+
+    RAM_tab = Frame(tabsys1) 
+    tabsys1.add(RAM_tab, text = "RAM")
+    
+    ram_g = GraphPage(RAM_tab, "RAM", nb_points=1000)
+    ram_g.pack(fill = 'both')
+
+    l_ram = Label(RAM_tab, font=('Calibri', 14))
+    l_ram.pack()
+
+    CPU_tab = Frame(tabsys1)
+    tabsys1.add(CPU_tab, text = 'CPU')
+
+    cpu_g = GraphPage(CPU_tab,"CPU", nb_points=1000)
+    cpu_g.pack(fill = 'both')
+
+    l_cpu = Label(CPU_tab, font=('Calibri', 14))
+    l_cpu.pack()
+
+    Disk_tab = Frame(tabsys1)
+    tabsys1.add(Disk_tab, text = "Disk")
+
+    disk_g = GraphPage(Disk_tab, "Disk", nb_points=1000)
+    disk_g.pack(fill = 'both')
+
+    l_disk = Label(root, font=('Calibri', 14))
+    l_disk.pack()
+    #Cursor on graph...
+    crs_cpu = cursor(cpu_g.figure, hover=True)
+    crs_cpu.connect("add", lambda sel: sel.annotation.set_text(
+        f'{cpu_g.graph_name} : {round(sel.target[1], 2)}'
+    ))
+    crs_ram = cursor(ram_g.figure, hover=True)
+    crs_ram.connect("add", lambda sel: sel.annotation.set_text(
+        f'{ram_g.graph_name} : {round(sel.target[1], 2)}'
+    ))
+    crs_disk = cursor(disk_g.figure, hover=True)
+    crs_disk.connect("add", lambda sel: sel.annotation.set_text(
+        f'{disk_g.graph_name} : {round(sel.target[1], 2)}'
+    ))
+
+    #Threads to map each resource
+    t1 = td.Thread(target=monitor_cpu, args=(
+        l_cpu, cpu_g, lambda: flag_for_thread))
+    t1.start()
+    t2 = td.Thread(target=monitor_ram, args=(
+        l_ram, ram_g, lambda: flag_for_thread))
+    t2.start()
+    t3 = td.Thread(target=disk_usage, args=(
+        l_disk, disk_g, lambda: flag_for_thread))
+    t3.start()
+
+
+
+    
+    
 
 
 def on_closing():
@@ -18,7 +79,11 @@ if __name__ == '__main__':
     icon = PhotoImage(file="resource.png")
     root.iconphoto(False, icon)
     root.title("Windows Performance Manager")
-    cpu_g = GraphPage(root, "CPU", nb_points=1000)
+    tabsys = ttk.Notebook(root)
+    tabsys.pack(expand = 1, fill = 'both')
+    LocalTabtd = td.Thread(target = PrepareTab, args = ("Local", ))
+    LocalTabtd.start()
+    """ cpu_g = GraphPage(LocalTab,"CPU", nb_points=1000)
     cpu_g.grid(column=1, row=0)
     l_cpu = Label(root, font=('Calibri', 14))
     l_ram = Label(root, font=('Calibri', 14))
@@ -29,7 +94,8 @@ if __name__ == '__main__':
     disk_g = GraphPage(root, "Disk", nb_points=1000)
     disk_g.grid(column=3, row=0)
     l_disk = Label(root, font=('Calibri', 14))
-    l_disk.grid(column=3, row=2)
+    l_disk.grid(column=3, row=2) """
+    """ cpu_g, l_cpu, ram_g, l_ram, disk_g, l_disk = PrepareTab("Local")
     crs_cpu = cursor(cpu_g.figure, hover=True)
     crs_cpu.connect("add", lambda sel: sel.annotation.set_text(
         f'{cpu_g.graph_name} : {round(sel.target[1], 2)}'
@@ -51,6 +117,7 @@ if __name__ == '__main__':
     t2.start()
     t3 = td.Thread(target=disk_usage, args=(
         l_disk, disk_g, lambda: flag_for_thread))
-    t3.start()
+    t3.start() """
+    flag_for_thread = False #A flag to manage all threads
     root.protocol("WM_DELETE_WINDOW", on_closing)
     mainloop()
